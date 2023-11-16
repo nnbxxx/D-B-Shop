08fbb54bd4c6680dc6cbfeda5c13ed36d55d62c5
nnbxxx <uyenbao4a5@gmail.com>
2023-11-16 17:19:46 +0700

    update page login

---
+++ target/classes/static/js/loginAdmin.js
@@ -0,0 +1,24 @@
+const container = document.getElementById("container");
+const registerBtn = document.getElementById("register");
+const loginBtn = document.getElementById("login");
+const loginAdminBtn = document.getElementById("btnGoAdmin");
+loginAdminBtn.addEventListener("click", () => {
+  window.location.href = "/alogin";
+});
+
+registerBtn.addEventListener("click", () => {
+  container.classList.add("active");
+});
+
+loginBtn.addEventListener("click", () => {
+  container.classList.remove("active");
+});
+
+const handleGotoLoginAdminPage = () => {
+  action = "/alogin";
+  console.log(
+    "🚀 ~ file: loginAdmin.js:15 ~ handleGotoLoginAdminPage ~ action:",
+    action
+  );
+  window.location.href = action;
+};
